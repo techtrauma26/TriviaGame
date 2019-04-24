7fabdeb030688400c03f2b1d6b2d6389488c50af
Vyjoo Frank <vlakshm26@gmail.com>
2019-04-24 10:22:50 -0400

    pushed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,166 +1,164 @@
 $(document).ready(function () {
 
-      const questions = [{
-          question: "What is the capital of Madagascar?",
-          answers: {
-            a: "Atoll",
-            b: "Nairobi",
-            c: "Masern",
-            d: "Antananarivo"
-          },
-          correctAnswer: "d"
-        },
-        {
-          question: "What is the capital of Mali?",
-          answers: {
-            a: "Amman",
-            b: "Dublin",
-            c: "Bamako",
-            d: "Vanduz"
-          },
-          correctAnswer: "c"
-        },
-        {
-          question: "What is the capital of Kyrgyzstan?",
-          answers: {
-            a: "Masern",
-            b: "Bishkek",
-            c: "Baghdad",
-            d: "Jakarta"
-          },
-          correctAnswer: "b"
-        },
-        {
-          question: "What is the capital of Morocco?",
-          answers: {
-            a: "Manila",
-            b: "Nairobi",
-            c: "Atoll",
-            d: "Rabat"
-          },
-          correctAnswer: "d"
-        },
-      ];
-      var playerAnswer = [];
+  // Variables // 
+  const questions = [
+    {
+      question: "1. What is the capital of Madagascar?",
+      answers: ["Atoll", "Nairobi", "Masern", "Antananarivo"],
+      correctAnswer: "Antanarivo"
+    },
+    {
+      question: "2. What is the capital of Mali?",
+      answers: ["Amman", "Dublin", "Bamako", "Vanduz"],
+      correctAnswer: "Bamako"
+    },
+    {
+      question: "3. What is the capital of Kyrgyzstan?",
+      answers: ["Masern", "Bishkek", "Baghdad", "Jakarta"],
+      correctAnswer: "Bishkek"
+    },
+    {
+      question: "4. What is the capital of Morocco?",
+      answers: ["Manila", "Nairobi", "Atoll", "Rabat"],
+      correctAnswer: "Rabat"
+    },
+  ];
 
-      var correct = 0;
-      var incorrect = 0;
-      var missed = 0;
+  // Create Questions and radio buttons // 
+  function showQ() {
+    const questionAsked = $("#content");
+    for (var i = 0; i < questions.length; i++) {
 
-      const content = document.getElementById("trivia");
-      const results = document.getElementById("yourResults");
-      const submitBtn = document.getElementById("submit");
+      questionAsked.append('<div id="question">' + questions[i].question + '</div>');
+
+      questionAsked.append('<div class="form-check"><input class="form-check-input" type="radio" name="radio-group' + i + '" id="radio' + i + '"><label class="form-check-label" id="radio' + i + 'label" for="radio' + i + '">' + questions[i].answers[0] + '</label></div>');
+      questionAsked.append('<div class="form-check"><input class="form-check-input" type="radio" name="radio-group' + i + '" id="radio' + i + '"><label class="form-check-label" id="radio' + i + 'label" for="radio' + i + '">' + questions[i].answers[1] + '</label></div>');
+      questionAsked.append('<div class="form-check"><input class="form-check-input" type="radio" name="radio-group' + i + '" id="radio' + i + '"><label class="form-check-label" id="radio' + i + 'label" for="radio' + i + '">' + questions[i].answers[2] + '</label></div>');
+      questionAsked.append('<div class="form-check"><input class="form-check-input" type="radio" name="radio-group' + i + '" id="radio' + i + '"><label class="form-check-label" id="radio' + i + 'label" for="radio' + i + '">' + questions[i].answers[3] + '</label></div>');
+    };
+  };
 
 
-      // ASSEMBLE LAYOUT OF THE GAME \\
-      function start() {
+  // Timer Function // 
+  var timerNum = 60;
 
-        const triviaGame = [];
 
-        questions.forEach(
-          (questionAsked, qNum) => {
-            const answers = [];
-            for (letter in questionAsked.answers) {
-              answers.push(
-                `<label> 
-        <input type="radio" name="question${qNum}" value="${letter}">
-            ${letter} :
-            ${questionAsked.answers[letter]}
-          </label>`
-              );
-            }
-            triviaGame.push(
-              `<div class="question"> ${questionAsked.question} </div>
-      <div class="answers"> ${answers.join('')} </div>`
-            );
-          }
-        );
-        content.innerHTML = triviaGame.join('');
+  function runTime() {
+    $("#timer").text("Time remaining: " + timerNum + " seconds");
+    timeInterval = setInterval(decrement, 1000);
+
+  };
+
+  function decrement() {
+    timerNum--;
+    console.log(timerNum);
+    $("#timer").text("Time remaining: " + timerNum + " seconds");
+
+    if (timerNum === 0) {
+      stop();
+      // checkResults();
+      alert("Time Up!");
+      $("#timer").empty();
+    };
+  };
+
+  function stop() {
+    clearInterval();
+    // clearInterval(setInterval(decrement, 1000));
+    checkResults();
+  };
+
+  // // Creating radio buttons //
+  // function showQ ()   {
+  //   const triviaGame = [];
+  //     questions.forEach(
+  //       (questionAsked, qNum) => {
+  //         const answers = [];
+
+  //         for (letter in questionAsked.answers) {
+  //           answers.push(
+  //             `
+  //             <form>
+  //             <label> 
+  //             <input type="radio" name="question${qNum}" value="${letter}">
+  //             ${letter} :
+  //             ${questionAsked.answers[letter]}
+  //             </label>
+  //             <form>
+  //             `
+  //           );
+  //         }
+  //         triviaGame.push(
+  //           `
+  //             <div id="question"> ${questionAsked.question} </div>
+  //             <div id="answers"> ${answers.join('')} </div>
+  //           `
+  //         );
+  //       }
+  //     );
+  //     content.innerHTML = triviaGame.join('');
+  // }
+
+  //Click StartBtn to Start Game//
+  function start() {
+    $('#startBtn').click(function () {
       runTime();
-      }
-
-      function submitAns() {
-        $("#submit").on("click", function (z) {
-          (this).preventDefault();
-          playerChoice.length = 0;
-
-          
-          // var playerSelection = $("#yourResults input:radio[name=choices]:checked").val().trim();
-          var playerSelection = $("#yourResults input:radio[name=choices]:checked").val().trim();
-          playerAnswer.push(playerSelection);
-          console.log(playerAnswer );
-        });
-      };
-
-
-        var timerNum = 100;
-        var timeInterval ;
-        function runTime() {
-          timeInterval = setInterval(decrement, 1000);
-
-        function decrement() {
-          timerNum--;
-        };
-          
-        if (timerNum === 0) {
-
-          stop();
-          checkResults();
-          alert("Time Up!");
-        }
-        
-        $("#timer").html("Time remaining: " + timerNum + " seconds");
-    
-
-      };
-
-
-
-   
-      runTime();
-
-  
-
-
-
-      // CHECK PLAYER'S ANSWERS & DISPLAY RESULTS \\
-    function checkResults() {
- 
-      const answerContainer = content.querySelectorAll(".answers");
- 
-    let correct = 0;
-
- 
-    questions.forEach(
-      (questionAsked, qNum) => {
-       
-      const results = answerContainers[qNum];
-      const selector = `input[name=question${qNum}]:checked`;
-      const userPicks = (answerContainer.querySelector(selector) || {}).value;
-
-      if (userPicks === questionAsked.correctAnswer) {
-        correct++;
-      } else {
-        incorrect++;
-           
-      }
+      document.getElementById("startBtn").style.display = "none";
+      decrement();
+      showQ();
+      $("#startBtn").hide;
     });
+  };
 
 
-  results.innerHTML = `${correct} out of ${questions.length}`;
-  }
+  // Submit Button to show results //
+  function submit() {
+    $('#submitBtn').click(function () {
+      stop();
+      checkResults();
+      document.getElementById("timer").style.display = "none";
+      // showresults();
+    });
+  };
+  function showresults(numC, numI, numU) {
+    $("#yourResults").show();
+    $("#timer").empty();
+    $("#timer").hide();
+    $("#content").hide();
+    $("#submitBtn").hide();
+    $("#correctA").text("# of Correct Answers: " + numC);
+    $("#incorrectA").text("# of Incorrect Answers: " + numI);
+    $("#unanswered").text("# of Questions Skipped : " + numU);
+    stop();
+  };
 
-      start();
-
-      submitBtn.addEventListener("click", checkResults);
-     
-      
 
 
+  function checkResults() {
+    var userA;
+    var numC = 0;
+    var numI = 0;
+    var numU = 0;
+    var correctA;
+    for (var i = 0; i < questions.length; i++) {
+      correctA = questions[i].correctAnswer;
+      userA = $('input[id=radio' + i + ']:checked + label').text();
+      console.log (userA);
+      console.log (correctA);
+      if (userA === correctA) {
+        numC++;
+      } else if (userA === "") {
+        numU++;
+      } else if (userA !== correctA) {
+        numI++;
+      };
+    };
+    showresults(numC, numI, numU);
+    
+  };
 
 
+  start();
+  submit();
 
-
-
-
-})
+});
